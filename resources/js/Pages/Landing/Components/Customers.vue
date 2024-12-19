68184e3a37b54c29701f284c70d0bd21a08882b3
Yisus Web <connexion.jesus@gmail.com>
2024-12-18 21:44:21 -0500

    hjhj

--- resources/js/Pages/Landing/Components/Customers.vue
+++ resources/js/Pages/Landing/Components/Customers.vue
@@ -22,11 +22,11 @@ const xassetUrl = assetUrl;
                                 data-filter="all">
                                 ALL</li>
                             <li class="text-[14px] text-[#17181c] 2xl:mx-[10px] sm:mx-[0px] px-[10px] leading-[11px] font-semibold hover:text-[#7963e0] cursor-pointer inline-block"
-                                data-filter=".design">DESIGN</li>
+                                data-filter=".corporativo">CORPORATIVO</li>
                             <li class="text-[14px] text-[#17181c] 2xl:mx-[10px] sm:mx-[0px] px-[10px] leading-[11px] font-semibold hover:text-[#7963e0] cursor-pointer inline-block"
-                                data-filter=".development">DEVELOPMENT</li>
+                                data-filter=".elerarning">E-LEARNING</li>
                             <li class="text-[14px] text-[#17181c] 2xl:mx-[10px] sm:mx-[0px] px-[10px] leading-[11px] font-semibold hover:text-[#7963e0] cursor-pointer inline-block"
-                                data-filter=".graphics">GRAPHICS</li>
+                                data-filter=".ecomerce">E-COMMERCE</li>
                             <li class="text-[14px] text-[#17181c] 2xl:mx-[10px] sm:mx-[0px] px-[10px] leading-[11px] font-semibold hover:text-[#7963e0] cursor-pointer inline-block"
                                 data-filter=".templates">Templates</li>
                         </ul>
@@ -37,19 +37,18 @@ const xassetUrl = assetUrl;
                                 <div class="portfolio-img truncate rounded-2xl relative">
                                     <img :src="`${xassetUrl}img/Landing/customers/Portafolio_Kentha.jpg`" style="width: 400px;"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
                                         <span
                                             class="bg-black rounded-full text-white font-normal text-[12px] px-2 py-1">3D
                                             Graphics</span>
                                         <span
                                             class="bg-black rounded-full text-white font-normal text-[12px] px-2 py-1">Templates</span>
-                                    </h3>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
-                                            <a href="#" class="text-white text-sm flex items-center">
-
-                                            </a>
+                                            <a href="#" class="text-white text-sm flex items-center">Boros -
+                                                Artificial Intelligence</a>
                                             <a :href="`${xassetUrl}img/Landing/customers/LogoKentha.png`" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
                                                 <p class="hidden">.</p>
@@ -66,11 +65,11 @@ const xassetUrl = assetUrl;
                                 <div class="portfolio-img truncate rounded-2xl relative">
                                     <img :src="`${xassetUrl}img/Landing/customers/Portafolio_ZoeLife.jpg`" alt="design"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
                                         <span
                                             class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Web
                                             Design</span>
-                                    </h3>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
@@ -92,11 +91,10 @@ const xassetUrl = assetUrl;
                                 <div class="portfolio-img truncate rounded-2xl relative">
                                     <img :src="`${xassetUrl}img/Landing/customers/Portafolio_Capperu.jpg`" alt="design"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
-                                        <span
-                                            class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Web
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                        <span class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Web
                                             Design</span>
-                                    </h3>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
@@ -116,12 +114,11 @@ const xassetUrl = assetUrl;
                             </div>
                             <div class="mix development">
                                 <div class="portfolio-img truncate rounded-2xl relative">
-                                    <img :src="`${xassetUrl}img/Landing/customers/LogoKentha.png`" alt="development"
+                                    <img :src="`${xassetUrl}img/Landing/customers/Portafolio_CelMovil.jpg`" alt="development"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
-                                        <span
-                                            class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Development</span>
-                                    </h3>
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                        <span class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Development</span>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
@@ -141,15 +138,15 @@ const xassetUrl = assetUrl;
                             </div>
                             <div class="mix templates design">
                                 <div class="portfolio-img truncate rounded-2xl relative">
-                                    <img :src="`${xassetUrl}img/Landing/customers/LogoKentha.png`" alt="templates"
+                                    <img :src="`${xassetUrl}img/Landing/customers/Portafolio_Despega.jpg`" alt="templates"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
                                         <span
                                             class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Templates</span>
                                         <span
                                             class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Web
                                             Design</span>
-                                    </h3>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
@@ -169,15 +166,15 @@ const xassetUrl = assetUrl;
                             </div>
                             <div class="mix development graphics">
                                 <div class="portfolio-img truncate rounded-2xl relative">
-                                    <img :src="`${xassetUrl}img/Landing/customers/LogoKentha.png`" alt="development"
+                                    <img :src="`${xassetUrl}img/Landing/customers/Portafolio_Despega.jpg`" alt="development"
                                         class="w-full transform hover:bg-blue-600 transition duration-500 hover:-rotate-12 hover:scale-125">
-                                    <h3 class="top-contain absolute top-[15px] right-[15px]">
+                                    <!-- <h3 class="top-contain absolute top-[15px] right-[15px]">
                                         <span
                                             class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">Development</span>
                                         <span
                                             class="bg-black rounded-full font-normal text-white text-[12px] px-2 py-1">3D
                                             Graphics</span>
-                                    </h3>
+                                    </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
                                             class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
