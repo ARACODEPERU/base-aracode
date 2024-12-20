e794c8ebea86956a1de9d6b64b9befab21f9be07
Yisus Web <connexion.jesus@gmail.com>
2024-12-19 23:22:47 -0500

    Pagina facturador y portadas

--- resources/js/Pages/Landing/Components/Customers.vue
+++ resources/js/Pages/Landing/Components/Customers.vue
@@ -1,7 +1,7 @@
 <script setup>
 const xassetUrl = assetUrl;
 
-import iconHome from '@/Components/vristo/icon/icon-home.vue';
+import iconEye from '@/Components/vristo/icon/icon-eye.vue';
 
 </script>
 <template>
@@ -13,7 +13,7 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                 ALGUNOS <span class="text-[#0188ee]"> PROYECTOS</span></h2>
         </div>
         <div
-            class="flex flex-wrap justify-between items-center mx-auto mx-auto 2xl:max-w-[1320px] xl:max-w-[1140px] lg:max-w-[960px] md:max-w-[720px] sm:max-w-[540px] max-[320px]:px-[12px] px-6">
+            class="flex flex-wrap justify-between items-center mx-auto 2xl:max-w-[1320px] xl:max-w-[1140px] lg:max-w-[960px] md:max-w-[720px] sm:max-w-[540px] max-[320px]:px-[12px] px-6">
             <div class="m-b-minus-24px w-full">
                 <div class="portfolio-content" id="MixItUpDA2FB7" data-aos="fade-up" data-aos-duration="2000"
                     data-aos-delay="600">
@@ -48,16 +48,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="https://kentha.cl/" class="text-white text-sm flex items-center">
-                                                kentha.cl
+                                                https://kentha.cl/
                                             </a>
                                             <a href="https://kentha.cl/" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
                                                 <p class="hidden"></p>
-                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center 
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
                                                 items-center rounded-md">
-                                                    <iconHome class="w-4 h-4 text-white font-medium" />
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -75,16 +75,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="https://zoelifeinternational.com/" class="text-white text-sm flex items-center">
-                                                ZoeLife International
+                                                https://zoelifeinternational.com/
                                             </a>
                                             <a href="https://zoelifeinternational.com/" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
-                                                <p class="hidden">.</p>
-                                                <span
-                                                    class="bg-[#0188ee] h-8 w-8 flex justify-center items-center rounded-md">
-                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                <p class="hidden"></p>
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
+                                                items-center rounded-md">
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -101,16 +101,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="#" class="text-white text-sm flex items-center">
                                                 https://capperu.com/
                                             </a>
                                             <a href="assets/img/portfolio/img-3.jpg" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
-                                                <p class="hidden">.</p>
-                                                <span
-                                                    class="bg-[#0188ee] h-8 w-8 flex justify-center items-center rounded-md">
-                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                <p class="hidden"></p>
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
+                                                items-center rounded-md">
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -126,16 +126,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="#" class="text-white text-sm flex items-center">
                                                 https://celmovil.pe/
                                             </a>
                                             <a href="assets/img/portfolio/img-4.jpg" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
-                                                <p class="hidden">.</p>
-                                                <span
-                                                    class="bg-[#0188ee] h-8 w-8 flex justify-center items-center rounded-md">
-                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                <p class="hidden"></p>
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
+                                                items-center rounded-md">
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -155,16 +155,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="#" class="text-white text-sm flex items-center">
                                                 https://marketingdespega.com/
                                             </a>
                                             <a href="assets/img/portfolio/img-1.jpg" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
-                                                <p class="hidden">.</p>
-                                                <span
-                                                    class="bg-[#0188ee] h-8 w-8 flex justify-center items-center rounded-md">
-                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                <p class="hidden"></p>
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
+                                                items-center rounded-md">
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -184,16 +184,16 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                                     </h3> -->
                                     <div class="bottom-contain absolute bottom-4 left-4 right-4">
                                         <div
-                                            class="overlay-info px-4 py-2 bg-black bg-opacity-60 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
+                                            class="overlay-info px-4 py-2 bg-black bg-opacity-90 rounded-xl grid grid-cols-2 gap-[30px] place-content-between">
                                             <a href="#" class="text-white text-sm flex items-center">
                                                 https://marketingdespega.com/login
                                             </a>
                                             <a href="assets/img/portfolio/img-2.jpg" data-fancybox="gallery"
                                                 class="text-white text-sm grid justify-items-end">
                                                 <p class="hidden">.</p>
-                                                <span
-                                                    class="bg-[#0188ee] h-8 w-8 flex justify-center items-center rounded-md">
-                                                    <i class="fa-solid fa-arrow-right"></i>
+                                                <span class="bg-[#0188ee] h-8 w-8 flex justify-center
+                                                items-center rounded-md">
+                                                    <icon-eye class=" w-5  h-5 text-white font-bold" />
                                                 </span>
                                             </a>
                                         </div>
@@ -203,6 +203,15 @@ import iconHome from '@/Components/vristo/icon/icon-home.vue';
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="flex justify-center">
+            <div>
+                <a href="#contact" type="button"
+                        class=" text-white bg-[#0188ee] hover:bg-[#f11600] no-underline font-medium rounded-full text-sm px-8 py-2.5 mr-2 hidden 2xl:block xl:block lg:block">
+                        Obtenga una cotización
+                </a>
             </div>
         </div>
     </section>
