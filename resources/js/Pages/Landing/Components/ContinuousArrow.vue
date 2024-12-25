fdc2f746d45cc78a2b23acfaba0a222eec1052ea
Yisus Web <connexion.jesus@gmail.com>
2024-12-25 12:37:24 -0500

    gyu

--- resources/js/Pages/Landing/Components/ContinuousArrow.vue
+++ resources/js/Pages/Landing/Components/ContinuousArrow.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex justify-center -mt-24">
-        <div class="animate-bounce border-2 border-[#0188ee] dark:bg-slate-800 p-2 w-20 h-20 ring-1 ring-slate-900/5 dark:ring-slate-200/20 
+        <div class="animate-bounce border-2 border-gray-400 dark:bg-slate-800 p-2 w-20 h-20 ring-1 ring-slate-900/5 dark:ring-slate-200/20 
             shadow-lg rounded-full flex items-center justify-center">
-            <svg class="w-8 h-8 text-[#0188ee]" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+            <svg class="w-8 h-8 text-gray-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
             </svg>
         </div>
