8a8c9d9d6ca913163d4259bb4799daa17e534b9a
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-01-27 13:39:55 -0500

    CertificadoAutomatico generator

--- routes/web.php
+++ routes/web.php
@@ -203,4 +203,8 @@ Route::middleware('auth')->group(function () {
     Route::post('meta/whatsapp/message/send', [MetaController::class, 'sendMessageWhatsapp'])->name('meta_whatsapp_message_send');
 });
 
+//CERTIFICADOS AUTOMATIZACIÓN Y PRUEBAS
+Route::get('/test-image/{student_id}/{certificate_id}', [WebController::class, 'testimage'])->name('test-image');
+
+
 require __DIR__ . '/auth.php';
