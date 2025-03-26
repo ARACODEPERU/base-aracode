95507d678e392a2d3f1441693ae727701070fbf2
ElRodriguez <elrodriguez2423@github.com>
2025-03-26 10:59:08 -0500

    ca

--- Modules/Sales/Resources/assets/js/Pages/Documents/List.vue
+++ Modules/Sales/Resources/assets/js/Pages/Documents/List.vue
@@ -380,7 +380,9 @@ const cancelDocument = (index, item) => {
                     <div class="flex gap-3">
                         <Keypad>
                             <template #botones>
-                                <Link :href="route('saledocuments_create')" class="inline-block px-6 py-2.5 bg-blue-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Nuevo</Link>
+                                <div v-can="'invo_documento_nuevo'">
+                                    <Link :href="route('saledocuments_create')" class="inline-block px-6 py-2.5 bg-blue-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Nuevo</Link>
+                                </div>
                             </template>
                         </Keypad>
 
