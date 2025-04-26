5fe7d986481637351e28b3f0e8e08bf96647bb4a
ElRodriguez <elrodriguez2423@github.com>
2025-04-25 19:14:48 -0500

    ss

--- Modules/Academic/Http/Controllers/AcaSaleDocumentController.php
+++ Modules/Academic/Http/Controllers/AcaSaleDocumentController.php
@@ -151,15 +151,17 @@ class AcaSaleDocumentController extends Controller
                         ////fin de actualizacion a la matricula
 
                     } else if ($product->entity_name_product == AcaSubscriptionType::class) {
-                        $curPro  = AcaSubscriptionType::where('id', $product->product_id)
-                            ->first();
+                        $curPro  = AcaSubscriptionType::where('id', $product->product_id)->first();
                         $origin = 'ACA';
                         $des =  $curPro->title . ' - ' . $curPro->description;
-                    } else {
-                        $curPro  = Product::where('id', $product->product_id)
-                            ->first();
+                    } else if ($product->entity_name_product == Product::class) {
+                        $curPro  = Product::where('id', $product->product_id)->first();
                         $origin = 'PRO';
-                        $des = $product->description;
+                        $des = $curPro->description;
+                    } else if ($product->entity_name_product == AcaCourse::class) {
+                        $curPro  = AcaCourse::where('id', $product->product_id)->first();
+                        $origin = 'ACA';
+                        $des = $curPro->description;
                     }
 
 
