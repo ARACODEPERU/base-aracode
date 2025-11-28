99e82149f884c333a944dac8134bf274b1579cf1
Yisus Web <connexion.jesus@gmail.com>
2025-11-28 03:30:01 -0500

    sdfdf

--- resources/views/components/cms-benefits.blade.php
+++ resources/views/components/cms-benefits.blade.php
@@ -30,7 +30,7 @@
                                 </div>
                             </div>
                             <div class="gradient-edge-bottom h-100"></div>
-                            <img src="{{ asset('themes/webpage/images/misc/s1.jpg') }}" class="w-100 hover-scale-1-1"
+                            <img src="{{ asset('themes/webpage/images/misc/cms01.jpg') }}" class="w-100 hover-scale-1-1"
                                 alt="">
                             <div class="abs w-100 h-100 start-0 top-0 hover-op-1 radial-gradient-color"></div>
                         </div>
@@ -53,7 +53,7 @@
                                 </div>
                             </div>
                             <div class="gradient-edge-bottom h-100"></div>
-                            <img src="{{ asset('themes/webpage/images/misc/s4.webp') }}" class="w-100 hover-scale-1-1"
+                            <img src="{{ asset('themes/webpage/images/misc/cms02.jpg') }}" class="w-100 hover-scale-1-1"
                                 alt="">
                             <div class="abs w-100 h-100 start-0 top-0 hover-op-1 radial-gradient-color"></div>
                         </div>
@@ -99,7 +99,7 @@
                                 </div>
                             </div>
                             <div class="gradient-edge-bottom h-100"></div>
-                            <img src="{{ asset('themes/webpage/images/misc/s6.webp') }}" class="w-100 hover-scale-1-1"
+                            <img src="{{ asset('themes/webpage/images/misc/cms04.jpg') }}" class="w-100 hover-scale-1-1"
                                 alt="">
                             <div class="abs w-100 h-100 start-0 top-0 hover-op-1 radial-gradient-color"></div>
                         </div>
