defc5e6d507e64a86948e7ce8ef3b570f43894f5
José Carlos <grande_josh@outlook.com>
2026-03-18 09:53:08 -0500

    s
    s

--- Modules/Academic/Operations/CertificateGeneratorHtml.php
+++ Modules/Academic/Operations/CertificateGeneratorHtml.php
@@ -71,11 +71,11 @@ class CertificateGeneratorHtml
                 ? null // No hacer nada especial
                 : $browsershot->setNodeBinary($rutaDeNode."node")->setNpmBinary($rutaDeNode."npm")->noSandbox() // <--- OBLIGATORIO al ejecutar como root o en Docker
                 ->setChromePath(env('RUTA_CHROMIUM')) // Usa el que instalamos con apt si el de npx falla;
-                ->addArgs([
-                    '--disable-setuid-sandbox',
-                    '--disable-dev-shm-usage',
-                    '--disable-extensions',
-                    '--no-zygote'
+                ->addChromiumArguments([
+                    'disable-setuid-sandbox',
+                    'disable-dev-shm-usage',
+                    'disable-extensions',
+                    'no-zygote'
                 ]);
             $browsershot->save($path);
 
