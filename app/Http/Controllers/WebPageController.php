2ed0df41f62f35ff1da43cfe91a7e176f4303530
José Carlos <grande_josh@outlook.com>
2026-04-29 13:27:44 -0500

    w
    w

--- app/Http/Controllers/WebPageController.php
+++ app/Http/Controllers/WebPageController.php
@@ -300,25 +300,24 @@ public function course_url_slug($id){
         }
 
         $colors = [
-            '#FF0000', // Rojo puro
-            '#00FF00', // Lima
-            '#0000FF', // Azul eléctrico
-            '#FFFF00', // Amarillo neón
-            '#FF00FF', // Magenta
-            '#00FFFF', // Cian
-            '#FF8C00', // Naranja oscuro
-            '#8A2BE2', // Azul violeta
-            '#ADFF2F', // Verde amarillo
-            '#FF1493', // Rosa profundo
-            '#00BFFF', // Azul cielo profundo
-            '#7FFF00', // Chartreuse
-            '#FF4500', // Naranja rojizo
-            '#1E90FF', // Azul esquivador
-            '#FFD700', // Oro
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
         ];
 
-        // Mezclamos el arreglo al azar
-        shuffle($colors);
 
         // Obtener OnliItem asociado al curso
         $onliItem = null;
@@ -376,25 +375,24 @@ public function course_url_slug($id){
             }
         }
         $colors = [
-            '#FF0000', // Rojo puro
-            '#00FF00', // Lima
-            '#0000FF', // Azul eléctrico
-            '#FFFF00', // Amarillo neón
-            '#FF00FF', // Magenta
-            '#00FFFF', // Cian
-            '#FF8C00', // Naranja oscuro
-            '#8A2BE2', // Azul violeta
-            '#ADFF2F', // Verde amarillo
-            '#FF1493', // Rosa profundo
-            '#00BFFF', // Azul cielo profundo
-            '#7FFF00', // Chartreuse
-            '#FF4500', // Naranja rojizo
-            '#1E90FF', // Azul esquivador
-            '#FFD700', // Oro
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
+            '#ff010b', // Rojo Institucional
+            '#575555', // Gris Institucional
         ];
 
-        // Mezclamos el arreglo al azar
-        shuffle($colors);
 
         // Obtener OnliItem asociado al curso
         $onliItem = null;
@@ -771,7 +769,6 @@ public function course_url_slug($id){
             // Manejar la excepción
             DB::rollback();
             $response = $e->getApiResponse();
-            dd($response); // Mostrar la respuesta para obtener más detalles
         }
 
 
@@ -957,7 +954,6 @@ public function course_url_slug($id){
             Auth::logout();
             DB::rollback();
             $response = $e->getApiResponse();
-            dd($response); // Mostrar la respuesta para obtener más detalles
         }
 
         return view('pages/pagar', [
@@ -1083,13 +1079,12 @@ public function course_url_slug($id){
                     ]);
                 } else {
 
+                    $sale->delete();
                     return response()->json([
                         'status' => $payment->status,
                         'message' => $payment->status_detail,
                         'url' => route('web_pagar')
                     ]);
-
-                    $sale->delete();
                 }
             } catch (\MercadoPago\Exceptions\MPApiException $e) {
                 // Manejar la excepción
